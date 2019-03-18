fde7ba574fdb3af969d1a0965af800d1905f76e2
NajamShehzad <najambutt195@gmail.com>
2019-03-18 11:59:19 +0500

    Setting Apollo For React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import BookList from './components/booklist/BookList';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client} >
+        <div >
+          <h1>
+            Apollo
+        </h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
---
+++ src/components/booklist/BookList.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
+const getBookQuery = gql`
+{
+  books{
+    name
+    _id
+    author{
+      name
+      age
+    }
+  }
+}
+
+`
+
+
+class BookList extends Component {
+
+    renderBooks = () => {
+        let data = this.props.data;
+        if (data.loading) {
+            return <div>Loading..</div>
+        } else {
+            return (
+                <ul>
+                    {data.books.map(bookData => {
+                        return <li key={bookData._id}>{bookData.name}</li>
+                    })}
+                </ul>
+            )
+
+        }
+    }
+
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                {this.renderBooks()}
+            </div>
+        )
+    }
+}
+
+
+export default graphql(getBookQuery)(BookList);
