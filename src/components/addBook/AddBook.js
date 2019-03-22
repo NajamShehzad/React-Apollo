6969947f8dd85e53070616d86772d47c39ebddd2
NajamShehzad <najambutt195@gmail.com>
2019-03-22 11:54:46 +0500

    Add Book Mutation Completed

--- src/components/addBook/AddBook.js
+++ src/components/addBook/AddBook.js
@@ -1,30 +1,97 @@
 import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import { gql } from 'apollo-boost';
+import { graphql, compose } from 'react-apollo';
+import { getAutherQuery, addBookMutation, getBookQuery } from '../../queries/queries';
 
-const getAutherQuery = gql`
-{
-  authors{
-    name
-    _id
-    books{
-      name
-    }
-  }
-}
-`
 
 class AddBook extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: "",
+            genre: "",
+            authorId: ""
+        }
+    }
+
+    showlist = () => {
+        let authorData = this.props.getAutherQuery;
+        if (!authorData.loading) {
+            return authorData.authors.map(authorData => {
+                return (
+                    <option value={authorData._id} key={authorData._id} >
+                        {authorData.name}
+                    </option>
+                )
+            })
+        } else {
+            return <option>Loading aurthors...</option>
+        }
+    }
+
+    onChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        console.log("submit")
+        console.log(this.state);
+        this.props.addBookMutation({
+            variables: {
+                name: this.state.name,
+                genre: this.state.genre,
+                authorId: this.state.authorId
+            },
+            refetchQueries: [{ query: getBookQuery }]
+        });
+    }
+
+
+
 
     render() {
-        console.log(this.props.data)
+        const { name, genre, authorId } = this.state;
+        console.log(this.props)
         return (
             <div>
-                Add Book Compoenet
+                <form id="add-form" onSubmit={this.onSubmit} >
+                    <div className="field" >
+                        <label>
+                            Book Name:
+                        </label>
+                        <input onChange={this.onChange} name="name" value={name} type="text" />
+                    </div>
+                    <div className="field">
+                        <label>
+                            Genre:
+                        </label>
+                        <input onChange={this.onChange} name="genre" value={genre} type="text" />
+                    </div>
+                    <div className="field">
+                        <label>
+                            Author:
+                        </label>
+                        <select onChange={this.onChange} name="authorId" value={authorId} >
+                            <option value="" >
+                                Select Aurthor
+                            </option>
+                            {this.showlist()}
+                        </select>
+                    </div>
+                    <div>
+                        <button type="submit" >
+                            Add Book
+                        </button>
+                    </div>
+                </form>
             </div>
         )
     }
 }
 
 
-export default graphql(getAutherQuery)(AddBook);
+export default compose(
+    graphql(getAutherQuery, { name: "getAutherQuery" }),
+    graphql(addBookMutation, { name: "addBookMutation" })
+)(AddBook);
--- src/components/booklist/BookList.js
+++ src/components/booklist/BookList.js
@@ -1,20 +1,6 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
-import { gql } from 'apollo-boost';
-
-const getBookQuery = gql`
-{
-  books{
-    name
-    _id
-    author{
-      name
-      age
-    }
-  }
-}
-
-`
+import { getBookQuery } from '../../queries/queries';
 
 
 class BookList extends Component {
